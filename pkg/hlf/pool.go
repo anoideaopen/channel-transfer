3d7f67cb0fd15c2c60724b03fb0297ca44f7484a
Alexander Zemtsov <a.zemtsov@gmail.com>
2024-09-12 20:29:57 +0300

    linter warning
    Signed-off-by: Alexander Zemtsov <a.zemtsov@gmail.com>

--- pkg/hlf/pool.go
+++ pkg/hlf/pool.go
@@ -112,7 +112,7 @@ func NewPool(
 }
 
 func newGRPCClient(opts *config.Batcher) (*grpc.ClientConn, error) {
-	var kacp = keepalive.ClientParameters{
+	kacp := keepalive.ClientParameters{
 		Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
 		Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
 		PermitWithoutStream: true,             // send pings even without active streams
